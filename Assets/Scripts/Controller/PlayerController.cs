a6bd3925bbc84f0052c4b99162fb39685024ad32
Rifai Kridolaksono <rivayx@gmail.com>
2023-03-28 04:47:24 +0700

    update controller for mounitng

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Cinemachine;
 
-
-namespace Sidji.TestProject.Controller
+namespace Sidji.TestProject.Controller.Player
 {
+    using Mounting;
+
     public class PlayerController : MonoBehaviour
     {
+        [Header("Character")]
         [SerializeField] GameObject characterObject;
         [SerializeField] Animator playerAnimator;
 
@@ -20,29 +23,87 @@ namespace Sidji.TestProject.Controller
         [SerializeField] Button buttonAttack;
 
         Transform cameraMain;
+        CinemachineFreeLook freeLook;
+        
+        GameObject mountingObject;
+        Animator mountingAnimator;
         
         Vector2 inputMovement;
         Vector3 movementPosition;
         float smoothTransition;
 
-        private void Start()
-        {
-            cameraMain = Camera.main.transform;
-            buttonAttack.onClick.AddListener(ExecuteAttack);
-        }
+        RigidbodyConstraints tempConstraintCharacter;
 
         private void OnEnable()
         {
+            cameraMain = Camera.main.transform;
+            freeLook = FindObjectOfType<CinemachineFreeLook>();
+
             joystick.OnJoystickInput += InputReceived;
+            buttonAttack.onClick.AddListener(ExecuteAttack);
+
+            tempConstraintCharacter = characterObject.GetComponent<Rigidbody>().constraints;
+
+            if (characterObject != null)
+            {
+                var colliderPlayerTrigger = characterObject.GetComponent<ColliderTrigger>();
+                colliderPlayerTrigger.OnColosionEnterToObject += OnCollisionEnterToPlayer;
+            }
         }
+
         private void OnDisable()
         {
             joystick.OnJoystickInput -= InputReceived;
+            buttonAttack.onClick.RemoveListener(ExecuteAttack);
+            
+            if (characterObject != null)
+            {
+                var colliderPlayerTrigger = characterObject.GetComponent<ColliderTrigger>();
+                colliderPlayerTrigger.OnColosionEnterToObject -= OnCollisionEnterToPlayer;
+            }
+        }
+
+        private void OnCollisionEnterToPlayer(Collision collision)
+        {
+            if (mountingObject != null) return;
+
+            if (collision.gameObject.GetComponent<MountingVariable>())
+            {
+                var mounting = collision.gameObject.GetComponent<MountingVariable>();
+                
+                playerAnimator.SetFloat("movement", 0);
+
+                mountingObject = mounting.MountingObject;
+                mountingAnimator = mounting.MountingAnimator;
+
+                freeLook.Follow = mountingObject.transform;
+                freeLook.LookAt = mountingObject.transform;
+
+                characterObject.transform.parent = mountingObject.transform;
+                characterObject.transform.localPosition = new Vector3(0, 2, 0);
+                characterObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
         }
 
         private void ExecuteAttack()
         {
-            playerAnimator.SetTrigger("attack");
+            if (mountingObject == null)
+            {
+                playerAnimator.SetTrigger("attack");
+            }
+            else
+            {
+                characterObject.transform.localPosition = mountingObject.transform.right;
+
+                characterObject.transform.parent = transform;
+                freeLook.Follow = characterObject.transform;
+                freeLook.LookAt = characterObject.transform;
+
+                mountingAnimator.SetFloat("movement", 0);
+                mountingObject = null;
+                mountingAnimator = null;
+            }
+
         }
 
         public void InputReceived(Vector2 input)
@@ -50,14 +111,25 @@ namespace Sidji.TestProject.Controller
             inputMovement = input;
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
             movementPosition = (cameraMain.forward * inputMovement.y + cameraMain.right * inputMovement.x);
             movementPosition.y = 0f;
+
             smoothTransition = Mathf.Lerp(smoothTransition, movementPosition.magnitude, Time.deltaTime * movementSpeed);
-            playerAnimator.SetFloat("movement", smoothTransition);
-            transform.position = Vector3.Lerp(transform.position, transform.position + movementPosition, Time.deltaTime * movementSpeed);
-            transform.forward = Vector3.Slerp(transform.forward, movementPosition, Time.deltaTime * movementSpeed);
+
+            if (mountingObject == null)
+            {
+                playerAnimator.SetFloat("movement", smoothTransition);
+                characterObject.transform.position = Vector3.Lerp(characterObject.transform.position, characterObject.transform.position + movementPosition, Time.deltaTime * movementSpeed);
+                characterObject.transform.forward = Vector3.Slerp(characterObject.transform.forward, movementPosition, Time.deltaTime * movementSpeed);
+            }
+            else
+            {
+                mountingAnimator.SetFloat("movement", smoothTransition);
+                mountingObject.transform.position = Vector3.Lerp(mountingObject.transform.position, mountingObject.transform.position + movementPosition, Time.deltaTime * movementSpeed);
+                mountingObject.transform.forward = Vector3.Slerp(mountingObject.transform.forward, movementPosition, Time.deltaTime * movementSpeed);
+            }
         }
     }
 }
